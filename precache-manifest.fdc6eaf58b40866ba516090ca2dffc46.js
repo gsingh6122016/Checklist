b42657eebac4e3146c40601584b57c1ac10f882d
gourav singh <gsingh6122016@gmail.com>
2020-08-14 10:40:51 +0530

    Updates

---
+++ precache-manifest.fdc6eaf58b40866ba516090ca2dffc46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44754dd371ce2f8f3f2ec75e5157632d",
+    "url": "/Cheklist/index.html"
+  },
+  {
+    "revision": "36be611101e84794a488",
+    "url": "/Cheklist/static/css/main.dda52c2f.chunk.css"
+  },
+  {
+    "revision": "2914fb7c3ebc4e35b9e2",
+    "url": "/Cheklist/static/js/2.f0e046a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cheklist/static/js/2.f0e046a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36be611101e84794a488",
+    "url": "/Cheklist/static/js/main.06f57998.chunk.js"
+  },
+  {
+    "revision": "366566d54ce8243b5dd1",
+    "url": "/Cheklist/static/js/runtime-main.06595a6a.js"
+  }
+]);
