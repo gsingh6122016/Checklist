96f382c86016f02363dbea2d53f2be1ad04c3c59
gourav singh <gsingh6122016@gmail.com>
2020-08-16 21:16:57 +0530

    a little cleaning of code

--- src/containers/ChecklistBuilder/ChecklistBuilder.js
+++ src/containers/ChecklistBuilder/ChecklistBuilder.js
@@ -9,101 +9,14 @@ import * as actions from '../../Store/actions/index';
 
 class ChecklistBuilder extends Component {
 
-    // state = {
-    //     // products: null,
-    //     totalPrice: 0,
-    //     spinner: true,
-    //     error: false,
-    //     totalCount: 0
-    //   }
-
-    // componentDidMount () {
-    //     axios.get('product')
-    //     .then(response => {
-
-    //         const products = response.data;
-           
-    //         const updatedproducts = products.map(product => {
-    //             return {
-    //                 ...product,
-    //                 count: 0
-    //             }
-    //         });
-
-    //         this.setState({products: updatedproducts});
-    //         this.setState({spinner: false});
-            
-    //     })
-    //     .catch(error => {
-    //     //    console.log(error);
-    //        this.setState({spinner: false});
-    //        this.setState({error: true})
-    //      } );
-    // }
-
   
 
     componentDidMount () {
-        console.log("fetch");
+     
         this.props.oninitProducts();
     }
    
  
-
-
-    // addProductHandler = (id) => {
-    //     const oldCount = this.state.products[id-1].count;
-    //     const updatedCount = oldCount + 1;
-
-    //     const products = [...this.state.products];
-       
-    //     const updatedproducts = products.map(product => {
-    //         return {
-    //             ...product,
-    //         }
-    //     });
-    //     updatedproducts[id-1].count = updatedCount;
-    //     const priceAdddition = parseInt(updatedproducts[id-1].price);
-    //         const oldPrice = this.state.totalPrice;
-    //         const newPrice = oldPrice + priceAdddition;
-
-    //     const oldTotalCount = this.state.totalCount;
-    //     const newTotalCount = oldTotalCount + 1;
-
-    //     this.setState({totalPrice: newPrice, products: updatedproducts, totalCount: newTotalCount})
-    // }
-
-    // removeProductHandler = (id) => {
-    //     const oldCount = this.state.products[id-1].count;
-    //     if (oldCount <= 0) {
-    //                 return;
-    //             }
-    //     const updatedCount = oldCount - 1;
-
-    //     const products = [...this.state.products];
-    //     const updatedproducts = products.map(product => {
-    //         return {
-    //             ...product
-    //         }
-    //     });
-    //     updatedproducts[id-1].count = updatedCount;
-    //     const priceAdddition = parseInt(updatedproducts[id-1].price);
-    //         const oldPrice = this.state.totalPrice;
-    //         const newPrice = oldPrice - priceAdddition;
-
-    //         const oldTotalCount = this.state.totalCount;
-    //         const newTotalCount = oldTotalCount - 1;
-
-    //     this.setState({totalPrice: newPrice, products: updatedproducts, totalCount: newTotalCount})
-    // }
- 
-//  productDetailsHandler = (id) => {
-//      this.setState({currentIndex: id-1, details: true});
-//  }
-
-    // detailCancelHandler = () => {
-    //     this.setState({details: false});
-    // }
 
     render() {
         
